1234e118202bbfdba2ed6b987b193dc3c54c6e33
SitiNurhalisa06 <115680505+SitiNurhalisa06@users.noreply.github.com>
2023-05-04 20:26:17 +0800

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\OrderController;
+
+Route::resource('orders', OrderController::class);
+
+Route::get('/books/trash', [BookController::class, 'trash'])->name('books.trash');
+Route::post('/books/{book}/restore', [BookController::class, 'restore'])->name('books.restore');
+Route::delete('/books/{id}/delete-permanent', [BookController::class, 'deletePermanent'])->name('books.delete-permanent');
+Route::resource('books', BookController::class);
+Route::get('/', function(){
+    return view('auth.login');
+});
+
+Auth::routes();
+Route::match(["GET", "POST"], "/register", function(){
+    return redirect("/login");
+})->name("register");
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource("users", UserController::class);
+Route::get('/categories/trash', [CategoryController::class, 'trash'])->name('categories.trash');
+Route::get('/categories/{id}/restore', [CategoryController::class, 'restore'])->name('categories.restore');
+Route::resource('categories', CategoryController::class);
+Route::delete('/categories/{category}/delete-permanent', [CategoryController::class, 'deletePermanent'])->name('categories.delete-permanent');
+Route::get('/ajax/categories/search', [CategoryController::class, 'ajaxSearch']);
